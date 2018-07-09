410515f60866aef887946e0bde1dd8be2a7a3d83
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2018-07-09 13:17:19 -0400

    Update dependencies, bump versions

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
  * @file All tests for module
  *
  * @author Ty-Lucas Kelley <ty@greylocklabs.com> (https://greylocklabs.com)
- * @copyright Copyright (c) 2017 Greylock Labs. See LICENSE file for details.
+ * @copyright Copyright (c) 2017-2018 Greylock Labs. See LICENSE file for details.
  */
 
 import test from 'ava';
